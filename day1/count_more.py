8daeefcbe9ce3a651819887eb28ffaa631999729
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-09 23:02:07 +0100

    refactored for variable window size

--- day1/count_more.py
+++ day1/count_more.py
@@ -1,36 +1,30 @@
 def count_more(file):
-    f = open(file, "r")
-    last = int(f.readline().strip())
-    count = 0
-    for next in f:
-        next = int(next.strip())
-        if next > last:
-            count += 1
-        last = next
-    return count
+    return count_avg(file,1)
 
-def count_avg(file):
+def count_avg(file, window):
     f = open(file, "r")
-    two_before_last = int(f.readline().strip())
-    before_last = int(f.readline().strip())
-    last = int(f.readline().strip())
-    last_sum = two_before_last + before_last + last
+    values = [0] * window
+    last_sum = 0
+    for i in range(window):
+        values[i] = int(f.readline().strip())
+        last_sum += values[i]
     count = 0
     for next in f:
         next = int(next.strip())
-        new_sum = next + last + before_last
+        new_sum = last_sum - values[0] + next
         if new_sum > last_sum:
             count += 1
-        before_last = last
-        last = next
+        for i in range(1,window):
+            values[i-1] = values[i]
+        values[window-1] = next
         last_sum = new_sum
     return count
 
 
 def main():
     print("the answer is ")
-    print(count_more("data/real_data_day1"))
-    print(count_avg("data/real_data_day1"))
+    print(count_more("data/real_data"))
+    print(count_avg("data/real_data",3))
 
 if __name__ == "__main__":
     main()
--- day1/tests/test_count_more.py
+++ day1/tests/test_count_more.py
@@ -1,9 +1,9 @@
 from day1.count_more import count_more, count_avg
 
 def test_count_more():
-    assert count_more("day1/data/testdata_day1") == 7
-    assert count_more("day1/data/real_data_day1") == 1448
+    assert count_more("day1/data/testdata") == 7
+    assert count_more("day1/data/real_data") == 1448
 
 def test_count_avg():
-    assert count_avg("day1/data/testdata_day1") == 5
-    assert count_avg("day1/data/real_data_day1") == 1471
+    assert count_avg("day1/data/testdata",3) == 5
+    assert count_avg("day1/data/real_data",3) == 1471
