8fe5025e07bfa39555f09fa7ac3f6c6c7a892e84
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-09 23:55:27 +0100

    initial set-up day3

---
+++ day3/get_power.py
@@ -0,0 +1,12 @@
+
+def get_power(file):
+    f = open(file, "r")
+    return 0
+
+def main():
+    print("the answer is ")
+    print(get_power("data/test_data"))
+    print(get_power("data/real_data"))
+
+if __name__ == "__main__":
+    main()
---
+++ day3/tests/test_get_power.py
@@ -0,0 +1,5 @@
+from day3.get_power import *
+
+def test_calc_coordinates():
+    assert get_power("day3/data/test_data") == 198
+   # assert get_power("day3/data/real_data") == 2215080
