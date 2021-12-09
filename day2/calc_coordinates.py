bbd316d1526dd5457f56e9ed8c010207d1eade31
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-09 23:42:12 +0100

    finished day 2

--- day2/calc_coordinates.py
+++ day2/calc_coordinates.py
@@ -4,19 +4,38 @@ def calc_coordinates(file):
     f = open(file, "r")
     for next in f:
         command = next.strip().split()
+        value = int(command[1])
         if command[0] == "forward":
-            x += int(command[1])
+            x += value
         elif command[0] == "down":
-            y += int(command[1])
+            y += value
         elif command[0] == "up":
-            y -= int(command[1])
+            y -= value
     return x * y
 
+def new_calc_coordinates(file):
+    x = 0
+    y = 0
+    aim = 0
+    f = open(file, "r")
+    for next in f:
+        command = next.strip().split()
+        value = int(command[1])
+        if command[0] == "forward":
+            x += value
+            y += aim * value
+        elif command[0] == "down":
+            aim += value
+        elif command[0] == "up":
+            aim -= value
+    return x * y
 
 def main():
     print("the answer is ")
     print(calc_coordinates("data/testdata"))
     print(calc_coordinates("data/real_data"))
+    print(new_calc_coordinates("data/testdata"))
+    print(new_calc_coordinates("data/real_data"))
 
 if __name__ == "__main__":
     main()
--- day2/tests/test_calc_coordinates.py
+++ day2/tests/test_calc_coordinates.py
@@ -3,3 +3,7 @@ from day2.calc_coordinates import *
 def test_calc_coordinates():
     assert calc_coordinates("day2/data/testdata") == 150
     assert calc_coordinates("day2/data/real_data") == 2215080
+
+def test_new_calc_coordinates():
+    assert new_calc_coordinates("day2/data/testdata") == 900
+    assert new_calc_coordinates("day2/data/real_data") == 1864715580
