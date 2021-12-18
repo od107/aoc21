98840b3af3321094dc7c97c418681c8d454516a5
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-18 15:09:50 +0100

    finished day5 part2

--- day5/tests/test_vent.py
+++ day5/tests/test_vent.py
@@ -5,6 +5,7 @@ def test_vents():
     assert vents("day5/data/test_data") == 5
     assert vents("day5/data/real_data") == 7438
 
-# def test_bingo_lose():
-#     assert lose_bingo("day5/data/test_data") == 1924
-#     assert lose_bingo("day5/data/real_data") == 8224
+
+def test_vent_diag():
+    assert vents("day5/data/test_data", False) == 12
+    assert vents("day5/data/real_data", False) == 21406
--- day5/vents.py
+++ day5/vents.py
@@ -1,5 +1,5 @@
 #
-def vents(file):
+def vents(file, only_straight=True):
     ground_map = [[0 for i in range(1000)] for j in range(1000)]
     with open(file) as f:
         while True:
@@ -12,12 +12,34 @@ def vents(file):
             line = [[int(coord) for coord in point if coord != ''] for point in line]
 
             if line[0][0] == line[1][0] or line[0][1] == line[1][1]:
-                add_path(line, ground_map)
+                add_straight_path(line, ground_map)
+            elif not only_straight:
+                add_diag_path(line, ground_map)
+
     score = calc_score(ground_map)
     return score
 
 
-def add_path(line, ground_map):
+def add_diag_path(line, ground_map):
+    if line[0][0] > line[1][0]:
+        line = [line[1], line[0]]
+    if line[0][1] < line[1][1]:
+        up = True
+    else:
+        up = False
+
+    start_x = line[0][0]
+    start_y = line[0][1]
+
+    if up:
+        for i in range(line[1][0] - line[0][0] + 1):
+            ground_map[start_x + i][start_y + i] += 1
+    else:
+        for i in range(line[1][0] - line[0][0] + 1):
+            ground_map[start_x + i][start_y - i] += 1
+
+
+def add_straight_path(line, ground_map):
     min_x = min(line[0][0], line[1][0])
     max_x = max(line[0][0], line[1][0])
     min_y = min(line[0][1], line[1][1])
@@ -41,8 +63,8 @@ def main():
     print("the answer is ")
     print(vents("data/test_data"))
     print(vents("data/real_data"))
-    # print(lose_bingo("data/test_data"))
-    # print(lose_bingo("data/real_data"))
+    print(vents("data/test_data", False))
+    print(vents("data/real_data", False))
 
 
 if __name__ == "__main__":
