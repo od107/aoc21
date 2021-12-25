a4bfacecfc7719651d3531fff71958ef244db297
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-25 17:04:15 +0100

    solved day10 part2

--- day10/syntax.py
+++ day10/syntax.py
@@ -1,73 +1,75 @@
 def syntax(file):
-    parentheses_dict = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-        "<": ">"
+    autocomplete_score = {
+        "(": 1,
+        "[": 2,
+        "{": 3,
+        "<": 4,
     }
+
     data = readfile(file)
-    score = 0
-    for line in data:
-        count_round = 0
-        count_square = 0
-        count_curly = 0
-        count_tag = 0
+    score_corrupt = 0
+    score_incomplete = []
+    for i, line in enumerate(data):
         last_chars = []
         for char in line:
             if char == "(":
-                count_round += 1
                 last_chars += char
             elif char == "[":
-                count_square += 1
                 last_chars += char
             elif char == "{":
-                count_curly += 1
                 last_chars += char
             elif char == "<":
-                count_tag +=1
                 last_chars += char
             elif char == ")":
                 if last_chars[-1] == "(":
-                    count_round -= 1
                     del last_chars[-1]
                 else:
-                    score += 3
+                    score_corrupt += 3
                     break
             elif char == "]":
                 if last_chars[-1] == "[":
-                    count_square -= 1
                     del last_chars[-1]
                 else:
-                    score += 57
+                    score_corrupt += 57
                     break
             elif char == "}":
                 if last_chars[-1] == "{":
-                    count_curly -= 1
                     del last_chars[-1]
                 else:
-                    score += 1197
+                    score_corrupt += 1197
                     break
             elif char == ">":
                 if last_chars[-1] == "<":
-                    count_tag -= 1
                     del last_chars[-1]
                 else:
-                    score += 25137
+                    score_corrupt += 25137
                     break
-    return score
+            elif char == "\n":
+                temp_score = 0
+                for item in reversed(last_chars):
+                    temp_score *= 5
+                    temp_score += autocomplete_score[item]
+                score_incomplete += [temp_score]
+
+    if len(score_incomplete) % 2 == 0:
+        raise Exception("length incomplete score is even")
+    score_incomplete.sort()
+    middle_idx = len(score_incomplete)//2
+    return score_corrupt, score_incomplete[middle_idx]
 
 
 def readfile(file):
     file_input = []
     with open(file) as f:
         while True:
-            text_line = f.readline()
-            if not text_line:
+            line = f.readline()
+            if not line:
                 break
-            line = text_line.strip()
+#            line = text_line.strip()
             line = [x for x in line]
             file_input += [line]
     return file_input
+
 
 def main():
     print("the answer is ")
--- day10/test_syntax.py
+++ day10/test_syntax.py
@@ -4,10 +4,19 @@ from day10.syntax import *
 
 class TestPart1(unittest.TestCase):
     def test_test_data(self):
-        self.assertEqual(syntax("data/test_data"), 26397)
+        self.assertEqual(26397, syntax("data/test_data")[0])
 
     def test_real_data(self):
-        self.assertEqual(syntax("data/real_data"), 411471)
+        self.assertEqual(411471, syntax("data/real_data")[0])
+
+
+class TestPart2(unittest.TestCase):
+    def test_test_data(self):
+        self.assertEqual(288957, syntax("data/test_data")[1])
+
+    def test_real_data(self):
+        self.assertEqual(syntax("data/real_data")[1], 3122628974)
+
 
 if __name__ == '__main__':
     unittest.main()
