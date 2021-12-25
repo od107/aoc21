c1f617549a89b3b7abbe0b7142a797963076093f
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-25 12:46:44 +0100

    solved day9 part 2

--- day9/height.py
+++ day9/height.py
@@ -1,6 +1,7 @@
 def height(file):
     height_map = readfile(file)
     lowest_values = []
+    low_points = []
 
     for y, line in enumerate(height_map):
         for x, value in enumerate(line):
@@ -13,8 +14,41 @@ def height(file):
             if y < len(height_map)-1 and height_map[y+1][x] <= value:
                 continue
             lowest_values += [value + 1]
+            low_points += [[x, y]]
 
-    return sum(lowest_values)
+    basin_sizes = []
+    for point in low_points:
+        basin_sizes += [find_basin_size(point, height_map)]
+    basin_sizes.sort(reverse=True)
+    basin_result = basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+
+    return sum(lowest_values), basin_result
+
+
+def find_basin_size(start_point, height_map):
+    basin_coordinates = [start_point]
+    to_process = basin_coordinates.copy()
+    map_max_x = len(height_map[0]) - 1
+    map_max_y = len(height_map) - 1
+
+    while to_process:
+        point = to_process.pop()
+        if point[0] != 0:
+            check_coordinate([point[0]-1, point[1]], basin_coordinates, to_process, height_map)
+        if point[0] < map_max_x:
+            check_coordinate([point[0]+1, point[1]], basin_coordinates, to_process, height_map)
+        if point[1] != 0:
+            check_coordinate([point[0], point[1]-1], basin_coordinates, to_process, height_map)
+        if point[1] < map_max_y:
+            check_coordinate([point[0], point[1]+1], basin_coordinates, to_process, height_map)
+
+    return len(basin_coordinates)
+
+
+def check_coordinate(point, basin_coordinates, to_process, height_map):
+    if point not in basin_coordinates and height_map[point[1]][point[0]] != 9:
+        to_process += [point]
+        basin_coordinates += [point]
 
 
 def readfile(file):
--- day9/tests/test_height.py
+++ day9/tests/test_height.py
@@ -2,10 +2,10 @@ from day9.height import *
 
 
 def test_part1():
-    assert height("day9/data/test_data") == 15
-    assert height("day9/data/real_data") == 550
+    assert height("day9/data/test_data")[0] == 15
+    assert height("day9/data/real_data")[0] == 550
 
 
-#def test_part2():
- #   assert total_output("day8/data/test_data") == 61229
-#    assert total_output("day8/data/real_data") == 1011284
+def test_part2():
+    assert height("day9/data/test_data")[1] == 1134
+    assert height("day9/data/real_data")[1] == 1100682
