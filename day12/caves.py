04129f82cc790964810ac55bd4c8d394a89d19f8
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-27 20:39:02 +0100

    solved day12 part 1

--- day12/caves.py
+++ day12/caves.py
@@ -12,35 +12,30 @@ def calc_paths(file):
             caves[line[0]].add_link(line[1])
             caves[line[1]].add_link(line[0])
 
-    paths = find_paths(caves)
+    paths = []
+    explore(caves, caves["start"], paths)
 
-    return paths
-
-
-def find_paths(caves):
-    paths = {}
-    to_process=[]
-    path = ["start"]
-    explore(caves, caves["start"], path)
-    return path, paths
+    return len(paths)
 
 
-# depth first search
-def explore(caves, cave, path):
-    end_found = False
+def explore(caves, cave, paths, path=None):
+    if not path:
+        path = []
+
+    path.append(cave.name)
+    if cave.name == "end":
+        print(path)
+        paths += [path.copy()]
+        return
+
     for link in cave.links:
-        if link.isupper() or link not in path and not end_found:
-            path.append(link)
-            if link != "end":
-                end_found = explore(caves, caves[link], path)
-            else:
-                return True
+        if link.isupper() or link not in path:
+            explore(caves, caves[link], paths, path.copy())
 
 
 class Cave:
     def __init__(self, name):
         self.name = name
- #       self.big = True if name.isupper() else self.big = False
         self.links = []
 
     def add_link(self, link):
@@ -62,9 +57,9 @@ def readfile(file):
 def main():
     print("the answer is ")
     print(calc_paths("data/simple_cave"))
-    # print(calc_paths("data/less_simple_cave"))
-    # print(calc_paths("data/larger_cave"))
-    # print(calc_paths("data/real_data"))
+    print(calc_paths("data/less_simple_cave"))
+    print(calc_paths("data/larger_cave"))
+    print(calc_paths("data/real_data"))
 
 
 if __name__ == "__main__":
--- day12/tests/test_caves.py
+++ day12/tests/test_caves.py
@@ -2,10 +2,10 @@ from day12.caves import *
 
 
 def test_part1():
-    assert calc_paths("day11/data/simple_cave") == 10
-    assert calc_paths("day11/data/less_simple_cave") == 19
-    assert calc_paths("day11/data/larger_cave") == 226
-    # assert calc_paths("day11/data/real_data") == 1667
+    assert calc_paths("day12/data/simple_cave") == 10
+    assert calc_paths("day12/data/less_simple_cave") == 19
+    assert calc_paths("day12/data/larger_cave") == 226
+    assert calc_paths("day12/data/real_data") == 3410
 
 #
 # def test_part2():
