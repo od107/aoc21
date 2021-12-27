579b4106c6ae08705ca1550851556eae2de0ec7b
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-27 18:24:55 +0100

    started with depth first search

--- day12/caves.py
+++ day12/caves.py
@@ -1,10 +1,50 @@
 
 
 def calc_paths(file):
+    caves = {}
     data = readfile(file)
 
-    
-    return number_of_paths
+    for line in data:
+        for item in line:
+            if item not in caves:
+                caves[item] = Cave(item)
+        if line[1] not in caves[line[0]].links:
+            caves[line[0]].add_link(line[1])
+            caves[line[1]].add_link(line[0])
+
+    paths = find_paths(caves)
+
+    return paths
+
+
+def find_paths(caves):
+    paths = {}
+    to_process=[]
+    path = ["start"]
+    explore(caves, caves["start"], path)
+    return path, paths
+
+
+# depth first search
+def explore(caves, cave, path):
+    end_found = False
+    for link in cave.links:
+        if link.isupper() or link not in path and not end_found:
+            path.append(link)
+            if link != "end":
+                end_found = explore(caves, caves[link], path)
+            else:
+                return True
+
+
+class Cave:
+    def __init__(self, name):
+        self.name = name
+ #       self.big = True if name.isupper() else self.big = False
+        self.links = []
+
+    def add_link(self, link):
+        self.links.append(link)
 
 
 def readfile(file):
