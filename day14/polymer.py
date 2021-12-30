b65f63d0abfd70d39e35c23cf8bfbdff1f2f0843
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-30 11:49:58 +0100

    solved day14 part2

--- day14/polymer.py
+++ day14/polymer.py
@@ -1,12 +1,15 @@
-def polymer(file, steps):
+from collections import Counter
+
+
+def slow_polymer(file, steps):
     template, inserts = readfile(file)
 
     for step in range(steps):
-        to_insert= []
+        to_insert = []
         for insert in inserts:
             for index, _ in enumerate(template):
-                if template.startswith(insert[0], index):
-                    to_insert += [[index+1, insert[1]]]
+                if template.startswith(insert, index):
+                    to_insert += [[index+1, inserts[insert]]]
         template = insert_in_template(template, to_insert)
 
     counts = {}
@@ -22,6 +25,32 @@ def polymer(file, steps):
     return maximum - minimum
 
 
+def fast_polymer(file, steps):
+    template, inserts = readfile(file)
+
+    # don't construct the string. Just keep track of the count of each 2-letter grouping
+    letterpair_template_count = Counter()
+    for i in range(1, len(template)):
+        letters = template[i-1]+template[i]
+        letterpair_template_count[letters] += 1
+
+    for step in range(steps):
+        # AB->C becomes AC, CB
+        transform_count = Counter()
+        for elem in letterpair_template_count:
+            if elem in inserts:
+                transform_count[elem[0] + inserts[elem]] += letterpair_template_count[elem]
+                transform_count[inserts[elem] + elem[1]] += letterpair_template_count[elem]
+        letterpair_template_count = transform_count
+
+    # count result
+    char_count = Counter()
+    for elem in letterpair_template_count:
+        char_count[elem[0]] += letterpair_template_count[elem]
+    char_count[template[-1]] += 1
+    return max(char_count.values())-min(char_count.values())
+
+
 def insert_in_template(template, to_insert):
     to_insert.sort(reverse=True)
     for index, char in to_insert:
@@ -30,7 +59,7 @@ def insert_in_template(template, to_insert):
 
 
 def readfile(file):
-    inserts = []
+    inserts = {}
 
     with open(file) as f:
         template = f.readline().strip()
@@ -40,16 +69,17 @@ def readfile(file):
             if not line:
                 break
             insert = line.strip().split(" -> ")
-            inserts += [insert]
+            inserts[insert[0]] = insert[1]
     return template, inserts
 
 
 def main():
     print("the answer is ")
-    print(polymer("data/test_data", 10))
-    print(polymer("data/real_data", 10))
-    # print(polymer("data/test_data", 40))
-    # print(polymer("data/real_data"; 40))
+    # print(slow_polymer("data/test_data", 10))
+    # print(slow_polymer("data/real_data", 10))
+    print(fast_polymer("data/test_data", 40))
+    # print(fast_polymer("data/test_data", 40))
+    print(fast_polymer("data/real_data", 40))
 
 
 if __name__ == "__main__":
--- day14/tests/test_polymer.py
+++ day14/tests/test_polymer.py
@@ -2,10 +2,13 @@ from day14.polymer import *
 
 
 def test_part1():
-    assert polymer("day14/data/test_data", 10) == 1588
-    assert polymer("day14/data/real_data", 10) == 2947
+    assert slow_polymer("day14/data/test_data", 10) == 1588
+    assert slow_polymer("day14/data/real_data", 10) == 2947
 
 
 def test_part2():
-    # assert polymer("day14/data/test_data", 40) == 2188189693529
-    # assert polymer("day14/data/real_data", 40) == 2947
+    assert fast_polymer("day14/data/test_data", 1) == 1
+    assert fast_polymer("day14/data/test_data", 10) == 1588
+    assert fast_polymer("day14/data/real_data", 10) == 2947
+    assert fast_polymer("day14/data/test_data", 40) == 2188189693529
+    assert fast_polymer("day14/data/real_data", 40) == 3232426226464
