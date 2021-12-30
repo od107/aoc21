e641b2ab9d1439e506d7f8cf6556ddce0a1f4d93
Jochen Deferme <jochen.deferme@gmail.com>
2021-12-30 16:55:09 +0100

    solved day15 part1 & tweaked performance

--- day15/chiton.py
+++ day15/chiton.py
@@ -1,5 +1,6 @@
-from collections import Counter, deque
-import operator
+import sys
+from collections import Counter, deque, defaultdict
+import operator, heapq
 
 
 def lowest_risk(file):
@@ -8,62 +9,90 @@ def lowest_risk(file):
     global dest
     dest = (len(data[0])-1, len(data)-1)
 
-    #implement A*
-    start = Node(0, 0)
-    to_process = [start]
-    nodes_considered = []
+    pqueue = []
+    considered_list = []
+    entry_finder = defaultdict(def_value)
+    for y, row in enumerate(data):
+        for x, risk in enumerate(row):
+            new_node = Node(x, y, risk)
+            pqueue.append(new_node)
+            entry_finder[(x, y)] = new_node
+    end_node = find(pqueue, dest[0], dest[1])
+    heapq.heapify(pqueue)
 
-    while to_process:
-        # current implementation delivers all points considered. Need to have backtracking
-        current_node = to_process.pop()
-        nodes_considered += [current_node]
-        if current_node.x == dest[0] and current_node.y == dest[1]:
+    while pqueue:
+        current_node = heapq.heappop(pqueue)
+        del entry_finder[(current_node.x, current_node.y)]
+        if current_node.removed:
+            continue
+        considered_list += [current_node]
+        if current_node == end_node:
             break
-        for x in range(max(0, current_node.x-1), min(dest[1], current_node.x+2)):
+        neighbors = []
+        for x in range(max(0, current_node.x-1), min(dest[1]+1, current_node.x+2)):
             if x == current_node.x:
                 continue
-            new_node = Node(x, current_node.y, current_node)
-            if new_node not in nodes_considered and new_node not in to_process:
-                to_process += [new_node]
+            # here was the performance gain
+            neighbor = entry_finder[(x, current_node.y)]
+            # neighbor = find(pqueue, x, current_node.y)
+            if neighbor:
+                neighbors += [neighbor]
 
-        for y in range(max(0, current_node.y-1), min(dest[1], current_node.y+2)):
+        for y in range(max(0, current_node.y-1), min(dest[1]+1, current_node.y+2)):
             if y == current_node.y:
                 continue
-            new_node = Node(current_node.x, y, current_node)
-            if new_node not in nodes_considered and new_node not in to_process:
-                to_process += [new_node]
-        to_process.sort(key=operator.attrgetter('g'))
+            # here was the performance gain
+            neighbor = entry_finder[(current_node.x, y)]
+            # neighbor = find(pqueue, current_node.x, y)
+            if neighbor:
+                neighbors += [neighbor]
 
-    # construct path via backtracking
-    current_node = nodes_considered[-1]
-    path = []
-    while current_node.parent is not None:
-        path.append(current_node)
-        current_node = current_node.parent
+        for neighbor in neighbors:
+            alt = current_node.dist + neighbor.risk
+            if alt < neighbor.dist:
+                # this update messes up the order of the priority queue
+                # using new_node and removed property instead of heapify doesn't help in performance
+                neighbor.removed = True
+                new_node = Node(neighbor.x, neighbor.y, neighbor.risk)
+                new_node.dist = alt
+                new_node.parent = current_node
+                heapq.heappush(pqueue, new_node)
+                entry_finder[(new_node.x, new_node.y)] = new_node
 
-    total_risk = 0
-    for node in path:
-        total_risk += node.f
-    return total_risk
+        # heapq.heapify(pqueue)
+
+    current_node = find(considered_list, dest[0], dest[1])
+    return current_node.dist
+
+
+def def_value():
+    return None
+
+
+def find(arr, x, y):
+    for el in arr:
+        if not el.removed and el.x == x and el.y == y:
+            return el
 
 
 class Node:
     global data
     global dest
 
-    def __init__(self, x, y, parent=None):
+    def __init__(self, x, y, risk):
+
         self.x = x
         self.y = y
-        self.parent = parent
-        if parent is None:
-            self.f = 0
-        else:
-            self.f = parent.f + data[y][x]
-        self.h = abs(dest[0]-x) + abs(dest[1]-y)
-        self.g = self.f + self.h
+        self.dist = sys.maxsize
+        self.parent = None
+        self.risk = risk
+        if x == 0 and y == 0:
+            self.dist = 0
+            self.risk = 0
+        self.removed = False
 
     def __lt__(self, other):
-        return self.g < other.g
+        return self.dist < other.dist
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
@@ -84,8 +113,9 @@ def readfile(file):
 
 def main():
     print("the answer is ")
+    # print(lowest_risk("data/simple_test_data"))
     print(lowest_risk("data/test_data"))
-    # print(lowest_risk("data/real_data"))
+    print(lowest_risk("data/real_data"))
 
 
 if __name__ == "__main__":
--- day15/tests/test_chiton.py
+++ day15/tests/test_chiton.py
@@ -2,8 +2,9 @@ from day15.chiton import *
 
 
 def test_part1():
+    assert lowest_risk("day15/data/simple_test_data") == 9
     assert lowest_risk("day15/data/test_data") == 40
-    # assert lowest_risk("day15/data/real_data") == 2947
+    assert lowest_risk("day15/data/real_data") == 441
 
 
 # def test_part2():
