6ba7408e25e0058d2e18953ebee293ebfb0f25b2
Jochen Deferme <jochen.deferme@gmail.com>
2022-01-04 14:48:05 +0100

    solved day16 part2

--- day16/decoder.py
+++ day16/decoder.py
@@ -1,20 +1,23 @@
+import math
 from collections import Counter, deque, defaultdict
 import sys, heapq, datetime
 
 
-def decode(file):
-    data = get_data(file)
+def decode(file, hex=None):
+    if file:
+        data = get_data(file)
+    else:
+        data = hex
     binary = bin(int(data, 16))[2:].zfill(4 * len(data))
 
-    idx = 0
-    versions = 0
-    idx, versions = parse(binary, idx, versions)
+    idx, versions, value = parse(binary, 0)
 
-    return versions
+    return versions, value
 
 
-def parse(binary, idx, versions):
-    versions += int(binary[idx:idx + 3], 2)
+def parse(binary, idx):
+    version = int(binary[idx:idx + 3], 2)
+    deeper_version = 0
     type_ID = int(binary[idx+3:idx+6], 2)
     idx += 6
     if type_ID == 4: #literal
@@ -26,7 +29,9 @@ def parse(binary, idx, versions):
             char = binary[idx:idx+5]
             idx += 5
         value = int(bin_value, 2)
-    else: #operator
+
+    else:  # operator
+        values = []
         length_type_ID = binary[idx]
         idx += 1
         if length_type_ID == "0":
@@ -34,13 +39,32 @@ def parse(binary, idx, versions):
             idx += 15
             end_pos = idx + nr_of_bits
             while idx < end_pos:
-                idx, versions = parse(binary, idx, versions)
+                idx, deeper_version, value = parse(binary, idx)
+                version += deeper_version
+                values.append(value)
         else:
             nr_of_packets = int(binary[idx:idx + 11], 2)
             idx += 11
             for i in range(nr_of_packets):
-                idx, versions = parse(binary, idx, versions)
-    return idx, versions
+                idx, deeper_version, value = parse(binary, idx)
+                version += deeper_version
+                values.append(value)
+        if type_ID == 0:
+            value = sum(values)
+        elif type_ID == 1:
+            value = math.prod(values)
+        elif type_ID == 2:
+            value = min(values)
+        elif type_ID == 3:
+            value = max(values)
+        elif type_ID == 5:
+            value = values[0] > values[1]
+        elif type_ID == 6:
+            value = values[0] < values[1]
+        elif type_ID == 7:
+            value = values[0] == values[1]
+
+    return idx, version, value
 
 
 def get_data(file):
@@ -56,14 +80,9 @@ def get_data(file):
 def main():
     print("the answer is ")
     # print(decode("data/simplest_test"))
-    print(decode("data/test_data"))
+    # print(decode("data/test_data_2"))
     print(decode("data/real_data"))
     # print(decode("data/test_data"))
-
-    # start_time = datetime.datetime.now()
-
-    # end_time = datetime.datetime.now()
-    # print(end_time - start_time)
 
 
 if __name__ == "__main__":
--- day16/tests/test_decoder.py
+++ day16/tests/test_decoder.py
@@ -2,13 +2,21 @@ from day16.decoder import *
 
 
 def test_part1():
-    assert decode("day16/data/test_data") == 16
-    assert decode("day16/data/test_data_2") == 12
-    assert decode("day16/data/test_data_3") == 23
-    assert decode("day16/data/test_data_4") == 31
-    assert decode("day16/data/real_data") == 979
+    assert decode("day16/data/test_data")[0] == 16
+    assert decode("day16/data/test_data_2")[0] == 12
+    assert decode("day16/data/test_data_3")[0] == 23
+    assert decode("day16/data/test_data_4")[0] == 31
+    assert decode("day16/data/real_data")[0] == 979
 
-#
-# def test_part2():
-#     assert lowest_risk("day15/data/test_data", True) == 315
-#     assert lowest_risk("day15/data/real_data", True) == 2849
+
+def test_part2():
+    assert decode(False, "C200B40A82")[1] == 3
+    assert decode(False, "04005AC33890")[1] == 54
+    assert decode(False, "880086C3E88112")[1] == 7
+    assert decode(False, "CE00C43D881120")[1] == 9
+    assert decode(False, "D8005AC2A8F0")[1] == 1
+    assert decode(False, "F600BC2D8F")[1] == 0
+    assert decode(False, "9C005AC2F8F0")[1] == 0
+    assert decode(False, "9C0141080250320F1802104A08")[1] == 1
+    assert decode("day16/data/real_data")[1] == 277110354175
+
