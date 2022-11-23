d7e093ca6cbc6d3762a598f41f3715105a8a5a87
Deferme <Jochen.Deferme@nike.com>
2022-11-23 11:31:39 +0100

    prepped tests

---
+++ day18/snailfish.py
@@ -0,0 +1,32 @@
+def add(file):
+
+    with open(file) as f:
+        value = f.readline().strip()
+
+    count = 0
+
+    return count
+
+
+def split(value):
+
+    return value
+
+
+def explode(value):
+
+    return value
+
+
+def magnitude(value):
+
+    return value
+
+
+def main():
+    print("the answer is ")
+    print(add("data/test_data1"))
+
+
+if __name__ == "__main__":
+    main()
